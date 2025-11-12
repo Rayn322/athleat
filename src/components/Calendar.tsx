a43197dbb14939646de108ec4e478e6db8cd3798
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2025-11-11 17:02:51 -0800

    HourSlot

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -42,7 +42,7 @@ function CalendarDay({
   );
 }
 
-export default function Calendar() {
+export function CalendarDayList() {
   return (
     <div className="flex items-center justify-between">
       {/* month is zero indexed */}
@@ -53,6 +53,57 @@ export default function Calendar() {
       <CalendarDay date={new Date(2025, 10, 14)} />
       <CalendarDay date={new Date(2025, 10, 15)} />
       <CalendarDay date={new Date(2025, 10, 16)} />
+    </div>
+  );
+}
+
+export function DaySchedule() {
+  // evil negative margin
+  // either make this a variable later or pay close attention to the layout's padding
+  return (
+    <div className="-mx-4 bg-bg-white">
+      <HourSlot hour={0} />
+      <HourSlot hour={1} />
+      <HourSlot hour={2} />
+      <HourSlot hour={3} />
+      <HourSlot hour={4} />
+      <HourSlot hour={5} />
+      <HourSlot hour={6} />
+      <HourSlot hour={7} />
+      <HourSlot hour={8} />
+      <HourSlot hour={9} />
+      <HourSlot hour={10} />
+      <HourSlot hour={11} />
+      <HourSlot hour={12} />
+      <HourSlot hour={13} />
+      <HourSlot hour={14} />
+      <HourSlot hour={15} />
+      <HourSlot hour={16} />
+      <HourSlot hour={17} />
+      <HourSlot hour={18} />
+      <HourSlot hour={19} />
+      <HourSlot hour={20} />
+      <HourSlot hour={21} />
+      <HourSlot hour={22} />
+      <HourSlot hour={23} />
+    </div>
+  );
+}
+
+function HourSlot({ hour = 8 }: { hour?: number }) {
+  const hourLabel =
+    hour === 0
+      ? "12 AM"
+      : hour < 12
+        ? `${hour} AM`
+        : hour === 12
+          ? "12 PM"
+          : `${hour - 12} PM`;
+
+  return (
+    // make pt 11px so the border adds the 12th pixel lol
+    <div className="border-t border-t-light-gray pt-2.75 pr-2.5 pb-15 pl-6">
+      <p className="text-small font-medium">{hourLabel}</p>
     </div>
   );
 }
