e503bee7addb5e2f957e306834a99d85de784ee2
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2025-11-12 16:46:15 -0800

    Layout with navbar and scrolling (hopefully) (#5)

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router";
+import { NavBar } from "../components/NavBar";
 
 export default function Layout() {
   return (
-    <div className="space-y-2 p-4">
-      <h1 className="text-2xl font-semibold">hello</h1>
-      <Outlet />
+    <div className="flex h-dvh flex-col">
+      <div className="overflow-y-auto px-4 pt-4">
+        <Outlet />
+      </div>
+      <NavBar active="home" />
     </div>
   );
 }
