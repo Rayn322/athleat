f39e3daa17f99f046f45556c623c64b00bfc13dd
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2025-11-16 19:54:46 -0800

    remove old imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import Home from "./pages/Home.tsx";
 import Layout from "./pages/Layout.tsx";
