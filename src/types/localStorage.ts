6b7c4bf134b7dc84c5620f7e75a04ce5c8f5dcd0
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2025-11-17 18:07:55 -0800

    Some types for state

--- src/types/localStorage.ts
+++ src/types/localStorage.ts
@@ -3,3 +3,27 @@ export interface User {
   email: string;
   password: string;
 }
+
+export interface Event {
+  id: string;
+  name: string;
+  days: boolean[]; // array of 7, bool for each day
+  startTime: string;
+  endTime: string;
+}
+
+export type Calendar = Event[];
+
+// add nutrition details if we have time
+export interface Meal {
+  name: string;
+  calories: number;
+  groceries: string[];
+}
+
+// since we aren't scheduling meals, heres 3 slots I guess
+export interface DayOfMeals {
+  breakfast: Meal;
+  lunch: Meal;
+  dinner: Meal;
+}
